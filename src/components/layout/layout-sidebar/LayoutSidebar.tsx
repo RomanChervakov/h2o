696a96832a232e53288c6ed8aa8dd36089007968
Roman Chervakov <61947236+RomanChervakov@users.noreply.github.com>
2025-05-16 03:46:58 +0300

    navigation

--- src/components/layout/layout-sidebar/LayoutSidebar.tsx
+++ src/components/layout/layout-sidebar/LayoutSidebar.tsx
@@ -1,5 +1,72 @@
 import styles from "./LayoutSidebar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faGear,
+  faUserFriends,
+  faCoins,
+  faChartPie,
+  faBoxArchive,
+  faRectangleList,
+} from "@fortawesome/free-solid-svg-icons";
+import logo from "../../../assets/logo.svg";
+import clsx from "clsx";
 
 export default function LayoutSidebar() {
-  return <nav className={styles.sidebar} />;
+  return (
+    <div className={styles.sidebar}>
+      <img src={logo} alt="logo" className={styles.logo} />
+      <nav className={styles.navigation}>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faCalendarDays}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faBoxArchive}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faRectangleList}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faUserFriends}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faCoins}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={clsx(styles.button, styles.active)}>
+          <FontAwesomeIcon
+            icon={faChartPie}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon
+            icon={faGear}
+            size="2x"
+            style={{ color: "#FFFFFF" }}
+          />
+        </button>
+      </nav>
+    </div>
+  );
 }
